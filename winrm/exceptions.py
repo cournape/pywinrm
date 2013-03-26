d88ebe086d5babba1c4ffb88981d58011939b93c
David Cournapeau <cournape@gmail.com>
2013-03-26 20:06:25 +0000

    BUG: fix exception hierarchy + add winrm time out exception.

--- winrm/exceptions.py
+++ winrm/exceptions.py
@@ -1,15 +1,29 @@
-class WinRMWebServiceError(Exception):
+class WinRMError(Exception):
+    pass
+
+class WinRMWebServiceError(WinRMError):
     """Generic WinRM SOAP Error"""
     pass
 
-class WinRMAuthorizationError(Exception):
+class WinRMAuthorizationError(WinRMError):
     """Authorization Error"""
     pass
 
-class WinRMWSManFault(Exception):
+class WinRMWSManFault(WinRMError):
     """A Fault returned in the SOAP response. The XML node is a WSManFault"""
+    def __init__(self, code, reason, detail):
+        super(WinRMWSManFault, self).__init__(code, reason, detail)
+        self.code = code
+        self.reason = reason
+        self.detail = detail
+
+    def __str__(self):
+        return self.reason
+
+class WinRMTimeout(WinRMWSManFault):
+    """Exception raised if the winrm request timed out (at the winrm level)."""
     pass
 
-class WinRMTransportError(Exception):
+class WinRMTransportError(WinRMError):
     """"Transport-level error"""
     pass
